02a94f2bb85bf5896e2b9425d010a4427f2ff5ec
Ashutosh Verma <ashutoshverma@cedcoss.com>
2022-05-31 19:26:19 +0530

    all pages added

--- src/app/controllers/AddProductController.php
+++ src/app/controllers/AddProductController.php
@@ -1,8 +1,29 @@
 <?php
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Http\Request;
+use Phalcon\Http\Response;
 
 class AddProductController extends Controller {
     public function indexAction() {
+    }
+
+    public function validateAction() {
+        $request = new Request();
+        if ( true === $request->isPost() ) {
+
+            if ( ! empty( $request->get('productData') ) ) {
+                $formData = $request->get( 'productData' );
+                $operation = 'product';
+            } elseif ( $request->get('orderData') ) {
+                $formData = $request->get( 'orderData' );
+                $operation = 'order';
+            } else {
+                $formData = array();
+                $operation = false;
+            }
+
+            
+        }
     }
 }
