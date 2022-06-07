7002986473c6533fb0ecc34ff9f53c4bdd8cfa06
Ashutosh Verma <ashutoshverma@cedcoss.com>
2022-06-07 12:03:34 +0530

    product list

--- src/app/controllers/ProductListController.php
+++ src/app/controllers/ProductListController.php
@@ -4,5 +4,7 @@ use Phalcon\Mvc\Controller;
 
 class ProductListController extends Controller {
     public function indexAction() {
+        $query = $this->modelsManager->createQuery("SELECT * FROM Products");
+        $this->view->products = $query->execute();
     }
 }
