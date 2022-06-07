07c0246f2c73352c4382c64db7916aaa1c775f8b
Ashutosh Verma <ashutoshverma@cedcoss.com>
2022-06-07 12:34:35 +0530

    deletion completed

--- src/app/controllers/ProductListController.php
+++ src/app/controllers/ProductListController.php
@@ -1,10 +1,31 @@
 <?php
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Http\Request;
 
 class ProductListController extends Controller {
     public function indexAction() {
         $query = $this->modelsManager->createQuery("SELECT * FROM Products");
         $this->view->products = $query->execute();
+    }
+
+    public function deleteAction() {
+
+        $request = new Request();
+        $pid     = $request->getQuery('pid');
+        $searchResults = Products::find(
+            [
+                'conditions' => 'id = :p_name:',
+                'bind'       => [
+                    'p_name' => $pid,
+                ]
+            ]
+        );
+
+        foreach ( $searchResults as $key => $product ) {
+            $product->delete();
+        }
+        
+        $this->response->redirect( 'product-list/' );
     }
 }
