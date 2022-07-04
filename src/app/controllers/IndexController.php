9b62f3531b897b64a24f083637977485eb1aea84
ced-ashutoshv <ashutoshverma@cedcoss.com>
2022-07-04 19:14:20 +0530

    finalised

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -7,9 +7,6 @@ use Phalcon\Http\Response;
 class IndexController {
 
     public function indexAction() {
-
-        AuthManager::createFirebaseAuth( 16 , 'admin' );
-
         try {
             throw new Exception("Invalid Request Url", 404);
         } catch (\Throwable $th) {
