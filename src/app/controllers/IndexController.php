e7fa6b88241ca95dedf2c60c4dafa25552f94c3b
ced-ashutoshv <ashutoshverma@cedcoss.com>
2022-07-13 12:37:12 +0530

    translation done

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -1,16 +1,51 @@
 <?php
-
-use Phalcon\Mvc\Controller;
+use Phalcon\Mvc\View;
 use Phalcon\Http\Request;
 use Phalcon\Http\Response;
+use Phalcon\Mvc\Controller;
 
-class IndexController {
+// Languages.
+use Phalcon\Translate\Adapter\NativeArray;
+use Phalcon\Translate\InterpolatorFactory;
+use Phalcon\Translate\TranslateFactory;
+
+class IndexController extends Controller {
 
     public function indexAction() {
-        try {
-            throw new Exception("Invalid Request Url", 404);
-        } catch (\Throwable $th) {
-            HttpManager::sendErrResponse( $th );
+        $translation         = $this->getTranslator();
+        $this->view->myName  = $translation->query( 'name' );
+        $this->view->appName = $translation->query( 'app' );
+    }
+
+    /**
+     * @return NativeArray
+     */
+    private function getTranslator(): NativeArray {
+
+        $request = new Request();
+
+        // Ask browser what is the best language.
+        $language = $request->getBestLanguage();
+
+        $messages = [];
+        
+        $translationFile = APP_PATH . '/messages/' . $language . '.php';
+
+        if ( true !== file_exists( $translationFile ) ) {
+            $translationFile = APP_PATH . '/messages/en.php';
         }
+        
+        require $translationFile;
+
+        $interpolator = new InterpolatorFactory();
+        $factory      = new TranslateFactory($interpolator);
+
+        return $factory->newInstance(
+            'array',
+            [
+                'content' => $messages,
+                'triggerError' => true,
+            ]
+        );
     }
 }
---
+++ src/app/messages/en.php
@@ -0,0 +1,5 @@
+<?php
+$messages = [
+    'name' => 'Ashutosh Verma',
+    'app'   => 'My first app',
+];
