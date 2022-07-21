9cefd6ea8055bf0f09560c6b9d203ee3b7ffb220
ced-ashutoshv <ashutoshverma@cedcoss.com>
2022-07-21 19:00:48 +0530

    oauth completed

--- src/app/controllers/SpotifyController.php
+++ src/app/controllers/SpotifyController.php
@@ -17,7 +17,9 @@ class SpotifyController extends Controller {
         $tokens = new Tokens();
         $this->view->tokens  = $tokens->find();
         $this->view->success = $this->request->get( 'success' );
-        $this->view->t       = $this->request->get( 't' );
+        $this->view->t       = $this->request->get( 't' );..
+
+        $token = $this->validateToken( $this->view->t );
     }
 
     public function callbackAction(){
@@ -105,7 +107,86 @@ class SpotifyController extends Controller {
         $this->response->redirect( $url );
     }
 
-    public function revokeAction(){
+    public function validateToken( int $id = null ){
+
+        $token = Tokens::findFirst( $id );
+        $created_at = $token->created_at;
+        $expired = ( $created_at + 3600 ) - time();
+        if( $expired < 0 ) { 
+          $expired = true;
+        } else {
+          $expired = false;
+        }
+
+        // Refresh Token.
+        if ( true === $expired ) {
+            $token = $this->refreshToken( $token );
+        }
+
+        return $token;
+    }
+
+    public function refreshToken( $token = null) {
         
+        if ( ! empty( $token ) ) {
+            $tokenUrl = $url = self::baseUrl . self::tokenEndpoint;
+            $curl     = curl_init();
+            $postFields = array(
+                'grant_type'  =>  'refresh_token',
+                'refresh_token'  =>  $token->refresh_token,
+            );
+            
+            $postFields = http_build_query($postFields, '', '&');
+            
+            curl_setopt_array(
+                $curl, array(
+                    CURLOPT_URL => $tokenUrl,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => $postFields,
+                    CURLOPT_HTTPHEADER => array(
+                        'Content-Type: application/x-www-form-urlencoded',
+                        'Authorization: Basic ' . base64_encode( self::clientId . ':' . self::secretId ),
+                    ),
+                )
+            );
+            
+            $response = curl_exec( $curl );
+            curl_close($curl);
+            $result = json_decode( $response, true );
+
+            if ( ! empty( $result[ 'error' ] ) ) {
+                
+                // Delete this token.
+                if ( 'Refresh token revoked' === $result['error_description'] ) {
+                    $token->delete();
+                }
+
+                $err = new Exception( $result[ 'error_description' ], 500 );
+                HttpManager::sendErrResponse( $err );
+            } else {
+                
+                $result['created_at'] = time();
+
+                $token->assign(
+                    $result,
+                    [
+                        'access_token',
+                        'token_type',
+                        'expires_in',
+                        'created_at',
+                    ]
+                );
+
+                $token->save();
+
+                if( empty( $token->id ) ) {
+                    $err = new Exception( 'Something Went wrong while saving the token', 400 );
+                    HttpManager::sendErrResponse( $err );
+                }
+            }
+        }
+
+        return $token;
     }
 }
